3dc98f4589c237c219086d04ab6d544e06221451
Shun Terabayashi <shunonymous@gmail.com>
2021-06-20 17:55:29 +0900

    Initial

---
+++ Customization.mk
@@ -0,0 +1,44 @@
+# Copyright (C) 2019 AngeloGioacchino Del Regno <kholk11@gmail.com>
+#
+# ROM specific customization for Sony Open Devices
+# PlatformConfig/BoardConfig overrides
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+CUST_PATH := device/sony/customization
+
+# Compilers setup for Q
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_CLANG_VERSION := r353983c
+
+# Data services
+USE_DEVICE_SPECIFIC_DATASERVICES := true
+
+# Kernel inline building
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+TARGET_KERNEL_SOURCE := $(KERNEL_PATH)/kernel
+TARGET_NEEDS_DTBOIMAGE := false
+
+#Enable enforcing SElinux
+BOARD_USE_ENFORCING_SELINUX := true
+
+ifneq ($(filter nile ganges yoshino, $(SOMC_PLATFORM)),)
+BOARD_KERNEL_SEPARATED_DTBO := false
+else
+BOARD_KERNEL_SEPARATED_DTBO := true
+endif
+
+TARGET_KERNEL_ADDITIONAL_FLAGS := \
+	DTC_EXT=/usr/bin/dtc
+
+include $(CUST_PATH)/LineageOS/Customization.mk
---
+++ customization.mk
@@ -0,0 +1,27 @@
+# Copyright (C) 2019 AngeloGioacchino Del Regno <kholk11@gmail.com>
+# Copyright (C) 2020 Martin Dünkelmann <nc-duenkekl3@netcologne.de>
+#
+# ROM specific customization for Sony Open Devices
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+TARGET_GAPPS_ARCH := arm64
+
+TARGET_KERNEL_HEADERS := kernel/sony/msm-4.14/kernel
+
+CUST_PATH := device/sony/customization
+
+# Updatable Apex
+DEXPREOPT_GENERATE_APEX_IMAGE := true
+
+include $(CUST_PATH)/LineageOS/customization.mk
